134224be17ebe59de860e42214bbc29119bae4ca
Gustavo Tenorio <gustavohst@msn.com>
2023-12-04 23:39:14 +0100

    Global css

--- ignite-feed/README.md
+++ ignite-feed/README.md
@@ -9,6 +9,8 @@ Currently, two official plugins are available:
 
 # IgniteFeed
 
+https://www.figma.com/file/fS0Vb8I9YMb5iZCxrjadNZ/Ignite-Feed-(Community)-(Copy)?type=design&node-id=0-1&mode=design&t=XrheSNDOTIzFXE6E-0
+
 Ignite project from rocket seat course
 
 Commands
--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -1,15 +1,17 @@
-import { Post } from "./Post"
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./global.css";
 
 function App() {
-
   return (
     <>
+      <Header></Header>
       <Post
         author="Gustavo Tenório"
         content="Gustavo is a man that wants achieve a lot of goals"
       />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
