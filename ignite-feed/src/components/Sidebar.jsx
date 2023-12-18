23c02c3c4407f9d5e4391bc54c910836728b7cf4
Gustavo Tenorio <gustavohst@msn.com>
2023-12-18 16:06:24 +0100

    Styling a sidebar

--- ignite-feed/src/components/Sidebar.jsx
+++ ignite-feed/src/components/Sidebar.jsx
@@ -1,5 +1,22 @@
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
-  return <aside className={styles.sidebar}></aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      ></img>
+
+      <div className={styles.profile}>
+        <img src="" alt="" />
+        <strong>Gustavo Tenório</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
 }
