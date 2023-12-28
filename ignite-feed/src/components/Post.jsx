3723558a0b7b7d4fdd78be0389b8c60b640511c2
Gustavo Tenorio <gustavohst@msn.com>
2023-12-28 20:37:44 +0100

    add date-fns library and adjust post component

--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -1,39 +1,57 @@
+import { format, formatDistanceToNow } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post(props) {
+const comment = [1, 2];
+
+export function Post({ author, publishedAt, content }) {
+  function handleCreateNewComent() {
+    event.preventDefault();
+    console.log("Teste");
+  }
+
+  const publishedDateFormatted = format(publishedAt, "LLLL d 'at' HH:mm'h'", {
+    locale: enUS,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: enUS,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/gustavohst.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Gustavo Tenório</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="May 11 at 08:33h" dateTime="2022-05-14 08:33:51">
-          Published at 1h
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
+
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="#"> jane.design/doctorcare </a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
-          <a href="#">#rocketseat</a>{" "}
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComent} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Let a comment" />
         <footer>
@@ -42,9 +60,9 @@ export function Post(props) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comment.map(() => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
